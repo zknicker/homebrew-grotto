2dff89e697254900fc07fdea63fbdedbc3791c04
Zach Knickerbocker <zknicker@gmail.com>
2026-06-11 18:16:48 -0400

    tavern-runtime 1.4.6

--- Formula/tavern-runtime.rb
+++ Formula/tavern-runtime.rb
@@ -1,9 +1,9 @@
 class TavernRuntime < Formula
   desc "Always-on Tavern Runtime server"
   homepage "https://github.com/zknicker/tavern"
-  url "https://punchpress-electron-app-209596837609-us-east-1-an.s3.us-east-1.amazonaws.com/tavern/mac/tavern-runtime-1.4.4-aarch64-apple-darwin.tar.gz"
-  sha256 "3f19640ce356144783c72274d2198b3f067b1969c170bfce6bddd31076a7be1c"
-  version "1.4.4"
+  url "https://punchpress-electron-app-209596837609-us-east-1-an.s3.us-east-1.amazonaws.com/tavern/mac/tavern-runtime-1.4.6-aarch64-apple-darwin.tar.gz"
+  sha256 "8b41201c90387329904f5db7decb6028f89e03aa60a2527e11bb04383be4b877"
+  version "1.4.6"
   license :cannot_represent
 
   depends_on "node"
@@ -14,7 +14,6 @@ class TavernRuntime < Formula
     (share/"tavern").install "share/tavern/runtime-assets"
     (share/"tavern/node_modules/@tavern").install "share/tavern/node_modules/@tavern/sdk"
     (etc/"tavern").mkpath
-    (var/"tavern/runtime").mkpath
     (var/"log/tavern").mkpath
   end
 
@@ -22,7 +21,6 @@ class TavernRuntime < Formula
     run [opt_bin/"tavern", "serve"]
     environment_variables TAVERN_RUNTIME_HOST: "127.0.0.1",
       TAVERN_RUNTIME_PORT: "18790",
-      TAVERN_RUNTIME_ROOT: var/"tavern/runtime",
       PATH: "#{HOMEBREW_PREFIX}/bin:#{HOMEBREW_PREFIX}/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
     keep_alive true
     log_path var/"log/tavern/runtime.log"
