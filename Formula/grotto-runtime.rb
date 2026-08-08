fcebd26444cdd61e562b8d8d41ff1cbe5633a6d4
Zach Knickerbocker <zknicker@gmail.com>
2026-08-08 19:25:41 -0400

    grotto-runtime 1.8.1

--- Formula/grotto-runtime.rb
+++ Formula/grotto-runtime.rb
@@ -1,9 +1,9 @@
 class GrottoRuntime < Formula
   desc "Always-on Grotto Runtime server"
   homepage "https://github.com/zknicker/grotto"
-  url "https://punchpress-electron-app-209596837609-us-east-1-an.s3.us-east-1.amazonaws.com/tavern/mac/grotto-runtime-1.8.0-aarch64-apple-darwin.tar.gz"
-  sha256 "7c186d29e5e519bc4d0d243e78748d14e717e212a0012a96d8cc6618554c2794"
-  version "1.8.0"
+  url "https://punchpress-electron-app-209596837609-us-east-1-an.s3.us-east-1.amazonaws.com/tavern/mac/grotto-runtime-1.8.1-aarch64-apple-darwin.tar.gz"
+  sha256 "de6a523182f1c1b60b014b197fc294dbb991fc9865aef08579c39cf3764142d5"
+  version "1.8.1"
   license :cannot_represent
 
   depends_on "node"
